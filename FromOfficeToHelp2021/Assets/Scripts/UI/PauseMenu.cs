0813674a2a7e79705ce55833372e6be05dbc38db
alejolacruz20 <92334009+alejolacruz20@users.noreply.github.com>
2021-10-30 19:11:31 -0300

    MenuPausa

---
+++ FromOfficeToHelp2021/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;
+    private bool pausedGame = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pausedGame = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pausedGame = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        pausedGame = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
