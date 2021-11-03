f11e0e6c489bbdd34b2d819ebe0350b44ddfd4f9
alejolacruz20 <92334009+alejolacruz20@users.noreply.github.com>
2021-11-03 15:29:32 -0300

    Cambio boton Pause Menu

--- FromOfficeToHelp2021/Assets/Scripts/UI/PauseMenu.cs
+++ FromOfficeToHelp2021/Assets/Scripts/UI/PauseMenu.cs
@@ -11,7 +11,7 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (pausedGame)
             {
