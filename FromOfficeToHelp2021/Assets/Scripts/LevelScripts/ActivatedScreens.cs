c2d379e5e70493eab06752ab906236b2168a44c8
alejolacruz20 <92334009+alejolacruz20@users.noreply.github.com>
2021-11-29 23:58:15 -0300

    Agregado Pantallas y Efectos Sala de Proyeccion

---
+++ FromOfficeToHelp2021/Assets/Scripts/LevelScripts/ActivatedScreens.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatedScreens : MonoBehaviour
+{
+    public GameObject[] screens;
+    ScreensButtons activatedScreens;
+    public List<GameObject> activatedScreensList;
+    public Animator[] npcAnim;
+    public Transform[] happyPointsSpawner;
+    public Renderer anotadorFinal;
+    public GameObject anotadorFinalObject;
+    public GameObject happyParticles;
+    bool dontSpawn = false;
+
+    void Start()
+    {
+        //anotadorFinal.enabled = false;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < screens.Length; i++)
+        {
+            activatedScreens = screens[i].GetComponent<ScreensButtons>();
+            if (activatedScreens.analyzed == false)
+            {
+                if (activatedScreens.activated == true)
+                {
+                    activatedScreensList.Add(screens[i]);
+                    activatedScreens.analyzed = true;
+                }
+            }
+        }
+
+        if (screens.Length == activatedScreensList.Count && anotadorFinalObject != null)
+        {
+            //anotadorFinal.enabled = true;
+            anotadorFinalObject.SetActive(true);
+
+            for (int j = 0; j < npcAnim.Length; j++)
+            {
+                npcAnim[j].SetBool("Happy", true);
+            }
+
+            if (!dontSpawn)
+            {
+                for (int k = 0; k < happyPointsSpawner.Length; k++)
+                {
+                    Instantiate(happyParticles, happyPointsSpawner[k].transform.position, happyPointsSpawner[k].transform.rotation);
+
+                    if(k == happyPointsSpawner.Length - 1)
+                    {
+                        dontSpawn = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FromOfficeToHelp2021/Assets/Scripts/LevelScripts/ScreensButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreensButtons : MonoBehaviour
+{
+    public bool activated;
+    public bool analyzed;
+    public Renderer crazyRend;
+    public Renderer happyRend;
+
+    void Start()
+    {
+        crazyRend.enabled = true;
+        happyRend.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.CompareTag("PlayerBullet"))
+        {
+            activated = true;
+            crazyRend.enabled = false;
+            happyRend.enabled = true;
+        }
+    }
+}
