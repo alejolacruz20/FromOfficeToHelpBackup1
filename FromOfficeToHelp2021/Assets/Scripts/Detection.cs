d6bdc84eb9559bcafa2d4c7fbbb3d10b664ecebc
Ligmabols <92348003+Ligmabols@users.noreply.github.com>
2022-02-17 21:35:53 -0300

    Testeo de camaras en una nueva escena
    avanace las camaras un poco en una escena nueva, solo tengo que arreglar el sistema ya que usa raycast y no se muy bien como usarlo

---
+++ FromOfficeToHelp2021/Assets/Scripts/Detection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    string playerTag;
+    Transform lente;
+    
+    void Start()
+    {
+        lente = transform.parent.GetComponent<Transform>();
+        playerTag = GameObject.FindGameObjectWithTag("Player").tag;
+    }
+
+    
+    void OntriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == playerTag)
+        {
+            Vector3 direction = col.transform.position - lente.position;
+            RaycastHit hit;
+
+            if (Physics.Raycast(lente.transform.position, direction.normalized, out hit, 1000))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
---
+++ FromOfficeToHelp2021/Assets/Scripts/TestCamara.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCamara : MonoBehaviour
+{
+    Transform camCuerpo;
+    bool startNextRot = true;
+    public bool rotRight;
+    public float yaw;
+    public float pitch;
+    public float secondsToRot;
+    public float rotSwitchTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camCuerpo = transform.GetChild(0);
+        camCuerpo.localRotation = Quaternion.AngleAxis(pitch, Vector3.right);
+        SetUpStartRotation();
+    }
+
+    void Update()
+    {
+        if (startNextRot && rotRight)
+        {
+            StartCoroutine(Rotate(yaw,secondsToRot));
+        }
+        else if (startNextRot && !rotRight)
+        {
+            StartCoroutine(Rotate(-yaw, secondsToRot));
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator Rotate(float yaw, float duration)
+    {
+        startNextRot = false;
+
+        Quaternion initialRotation = transform.rotation;
+
+        float timer = 0;
+
+        while (timer < duration) 
+        {
+            timer += Time.deltaTime;
+            transform.rotation = initialRotation * Quaternion.AngleAxis(timer / duration * yaw, Vector3.up);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(rotSwitchTime);
+
+        startNextRot = true;
+        rotRight = !rotRight;
+    }
+
+    void SetUpStartRotation() 
+    {
+        if (rotRight)
+        {
+            transform.localRotation = Quaternion.AngleAxis(-yaw / 2, Vector3.up);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.AngleAxis(yaw / 2, Vector3.up);
+        }
+    }
+}
